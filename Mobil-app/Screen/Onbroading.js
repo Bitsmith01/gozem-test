22a42e46131479b339c5a5919d0da4de5be05540
Bitsmith01 <esperat.dimon@epitech.eu>
2023-10-18 15:16:29 +0100

    Setting Onbroading page

--- Mobil-app/Screen/Onbroading.js
+++ Mobil-app/Screen/Onbroading.js
@@ -1,33 +1,70 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { ImageBackground, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-const Onbroading = () => {
+const Onboarding = ({ navigation }) => {
+
+  const handleInscriptionClick = () => {
+    navigation.navigate('Register');
+  };
+
   return (
-    <View style={styles.container}>
-      <Image style={styles.Topimage} source={require('../assets/who-we-are2x.png')} />
-    </View>
-  )
+    <ImageBackground source={require('../assets/who-we-are2x.png')} style={{ flex: 1, justifyContent: "flex-end" }}>
+      <LinearGradient
+        colors={['transparent', '#179138']}
+        style={styles.Linear}
+      >
+        <Text style={styles.Welcome}>
+          Bienvenue champion !
+        </Text>
+        <Text style={styles.subtitle}>
+          Rejoins notre communauté de livreurs et booste tes revenus de manière exceptionnelle. 
+          Prépare-toi pour une aventure passionnante ! Rejoins-nous dès maintenant !
+        </Text>
+        <TouchableOpacity style={styles.inscription} onPress={handleInscriptionClick}>
+          <Text>
+            Inscription
+          </Text>
+        </TouchableOpacity>
+      </LinearGradient>
+    </ImageBackground>
+  );
 }
-
-export default Onbroading
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'start',
+    alignItems: '',
+    justifyContent: 'flex-end',
   },
-  Topimage: {
-    objectFit: 'cover',
-    right:20
+  subtitle: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 20
   },
-  BottomSection: {
-    paddingTop: 40,
-    marginTop: -20,
+  Linear: {
+    height: "70%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 250
+  },
+  Welcome: {
+    fontSize: 35,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  inscription: {
     backgroundColor: 'white',
-    width: "100%",
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20
+    height: 39,
+    width: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20
   }
-})
+});
+
+export default Onboarding;
